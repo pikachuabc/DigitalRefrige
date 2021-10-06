01022b4dd021a115e4757f3860b54df658f3ea51
Jiajun Liu <jiajunliu0024@gmail.com>
2021-10-07 00:44:19 +1100

    add the add label function

--- app/src/main/java/com/example/digitalrefrige/views/labelList/LabelListFragment.java
+++ app/src/main/java/com/example/digitalrefrige/views/labelList/LabelListFragment.java
@@ -4,19 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.digitalrefrige.databinding.FragmentLabelListBinding;
 import com.example.digitalrefrige.databinding.FragmentTheOtherBinding;
+import com.example.digitalrefrige.model.dataHolder.Item;
 import com.example.digitalrefrige.model.dataHolder.Label;
 import com.example.digitalrefrige.model.dataQuery.LabelWithItems;
 import com.example.digitalrefrige.viewModel.LabelListViewModel;
 import com.example.digitalrefrige.viewModel.adapters.LabelListAdapter;
+import com.example.digitalrefrige.views.itemList.ItemDetailFragmentArgs;
 
 import java.util.List;
 
@@ -28,16 +34,18 @@ public class LabelListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        labelListViewModel = new ViewModelProvider(requireActivity()).get(LabelListViewModel.class);
+
         binding = FragmentLabelListBinding.inflate(inflater, container, false);
         RecyclerView labelListRecyclerView = binding.labelListRecyclerView;
         labelListRecyclerView.setLayoutManager(new LinearLayoutManager(this.getContext()));
         labelListRecyclerView.setHasFixedSize(true);
         final LabelListAdapter labelListAdapter = new LabelListAdapter();
         labelListRecyclerView.setAdapter(labelListAdapter);
-
+        binding.buttonAddLabel.setOnClickListener(this::onAddButtonClicked);
 
         // inject viewModel and start observing
+        labelListViewModel = new ViewModelProvider(requireActivity()).get(LabelListViewModel.class);
+
 
         labelListViewModel.getAllLabels().observe(getViewLifecycleOwner(), new Observer<List<Label>>() {
             @Override
@@ -53,10 +61,35 @@ public class LabelListFragment extends Fragment {
             }
         });
 
+        // enable swipe delete on our recyclerView
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                Label toDelete = labelListAdapter.getCurrentList().get(viewHolder.getAdapterPosition());
+                labelListViewModel.deleteLabel(toDelete);
+                Toast.makeText(getContext(), "label deleted", Toast.LENGTH_SHORT).show();
+            }
+        }).attachToRecyclerView(labelListRecyclerView);
 
 
 
         return binding.getRoot();
 
     }
+
+    public void onAddButtonClicked(View view) {
+        binding.labelEditText.clearFocus();
+        long r = labelListViewModel.insertLabel(new Label(binding.labelEditText.getText().toString()));
+        if(r == -1L){
+            Toast.makeText(getContext(), "failed to add", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(getContext(), "label added", Toast.LENGTH_SHORT).show();
+        }
+    }
+
 }
