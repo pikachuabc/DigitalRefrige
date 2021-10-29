51ec63dd73e5be1b8b8c916ac7e7279ec157c4b6
Xingdong Cai <cxd2604@gmail.com>
2021-10-29 17:20:07 +1100

    fix image fir issue

--- app/src/main/java/com/example/digitalrefrige/views/login/SyncFragment.java
+++ app/src/main/java/com/example/digitalrefrige/views/login/SyncFragment.java
@@ -10,6 +10,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -22,6 +23,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -56,8 +58,10 @@ import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 public class SyncFragment extends Fragment {
@@ -71,7 +75,6 @@ public class SyncFragment extends Fragment {
     private Converters c;
     private OutputStream outputStream;
     FirebaseUser user;
-
 
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -93,7 +96,7 @@ public class SyncFragment extends Fragment {
         return binding.getRoot();
     }
 
-    public void uploadData(){
+    public void uploadData() {
 
         uploadItemTable();
         uploadLabelTable();
@@ -112,7 +115,7 @@ public class SyncFragment extends Fragment {
         updateTime(uploadTime);
     }
 
-    public void fetchData(){
+    public void fetchData() {
 
         fetchItemTable();
         fetchLabelTable();
@@ -132,7 +135,7 @@ public class SyncFragment extends Fragment {
 
     }
 
-    public void uploadItemTable(){
+    public void uploadItemTable() {
         // get user's cloud items
         // check which of them are not in the local database, and remove
 
@@ -147,7 +150,7 @@ public class SyncFragment extends Fragment {
                         HashMap<Item, String> cloudItems = new HashMap<>();
                         for (QueryDocumentSnapshot document : task.getResult()) {
                             cloudItems.put(document.toObject(Item.class), document.getId());
-                            if(!localItems.contains(document.toObject(Item.class))){
+                            if (!localItems.contains(document.toObject(Item.class))) {
                                 db.collection("item_table").document(document.getId()).delete();
                             }
                             Log.d(TAG, document.getId() + " => " + document.getData());
@@ -156,7 +159,7 @@ public class SyncFragment extends Fragment {
                         // For every local item, check whether it is in cloud
                         // if it not, add
                         // if it is,  set
-                        for(Item i: localItems){
+                        for (Item i : localItems) {
                             Map<String, Object> item = new HashMap<>();
                             item.put("name", i.getName());
                             item.put("description", i.getDescription());
@@ -166,21 +169,21 @@ public class SyncFragment extends Fragment {
                             item.put("imgUrl", i.getImgUrl());
                             item.put("user", user.getUid());
 
-                            if(!cloudItems.keySet().contains(i)){
-                                db.collection("item_table").add(item).addOnCompleteListener( t-> {
+                            if (!cloudItems.keySet().contains(i)) {
+                                db.collection("item_table").add(item).addOnCompleteListener(t -> {
                                     if (t.isSuccessful()) {
                                         DocumentReference document = t.getResult();
-                                        if(i.getImgUrl().length() != 0){
+                                        if (i.getImgUrl().length() != 0) {
                                             convertImageUrlToCloud(i.getImgUrl(), i.getItemId(), document.getId());
                                         }
-                                    }else{
+                                    } else {
                                         System.out.print("Error adding documents");
                                     }
                                 });
-                            }else{
+                            } else {
                                 Toast.makeText(getContext(), "3333333333333", Toast.LENGTH_SHORT).show();
                                 db.collection("item_table").document(cloudItems.get(i)).set(item);
-                                if(i.getImgUrl().length() != 0){
+                                if (i.getImgUrl().length() != 0) {
                                     convertImageUrlToCloud(i.getImgUrl(), i.getItemId(), cloudItems.get(i));
                                 }
                             }
@@ -193,7 +196,7 @@ public class SyncFragment extends Fragment {
                 });
     }
 
-    public void uploadLabelTable(){
+    public void uploadLabelTable() {
         // get user's cloud items
         // check which of them are not in the local database, and remove
 
@@ -207,7 +210,7 @@ public class SyncFragment extends Fragment {
                         HashMap<Label, String> cloudLabels = new HashMap<>();
                         for (QueryDocumentSnapshot document : task.getResult()) {
                             cloudLabels.put(document.toObject(Label.class), document.getId());
-                            if(!localLabels.contains(document.toObject(Label.class))){
+                            if (!localLabels.contains(document.toObject(Label.class))) {
                                 db.collection("label_table").document(document.getId()).delete();
                             }
                             Log.d(TAG, document.getId() + " => " + document.getData());
@@ -216,14 +219,14 @@ public class SyncFragment extends Fragment {
                         // For every local item, check whether it is in cloud
                         // if it not, add
                         // if it is,  set
-                        for(Label i: localLabels){
+                        for (Label i : localLabels) {
                             Map<String, Object> label = new HashMap<>();
                             label.put("labelID", i.getLabelId());
                             label.put("title", i.getTitle());
                             label.put("user", user.getUid());
-                            if(!cloudLabels.keySet().contains(i)){
+                            if (!cloudLabels.keySet().contains(i)) {
                                 db.collection("label_table").add(label);
-                            }else{
+                            } else {
                                 db.collection("label_table").document(cloudLabels.get(i)).set(label);
                             }
                         }
@@ -234,7 +237,7 @@ public class SyncFragment extends Fragment {
                 });
     }
 
-    public void uploadCrossRefTable(){
+    public void uploadCrossRefTable() {
         // get user's cloud items
         // check which of them are not in the local database, and remove
 
@@ -248,7 +251,7 @@ public class SyncFragment extends Fragment {
                         HashMap<ItemLabelCrossRef, String> cloudRefs = new HashMap<>();
                         for (QueryDocumentSnapshot document : task.getResult()) {
                             cloudRefs.put(document.toObject(ItemLabelCrossRef.class), document.getId());
-                            if(!localRefs.contains(document.toObject(ItemLabelCrossRef.class))){
+                            if (!localRefs.contains(document.toObject(ItemLabelCrossRef.class))) {
                                 db.collection("itemlabelcrossref_table").document(document.getId()).delete();
                             }
                             Log.d(TAG, document.getId() + " => " + document.getData());
@@ -257,14 +260,14 @@ public class SyncFragment extends Fragment {
                         // For every local item, check whether it is in cloud
                         // if it not, add
                         // if it is,  set
-                        for(ItemLabelCrossRef i: localRefs){
+                        for (ItemLabelCrossRef i : localRefs) {
                             Map<String, Object> ref = new HashMap<>();
                             ref.put("labelID", i.getLabelId());
                             ref.put("itemID", i.getItemId());
                             ref.put("user", user.getUid());
-                            if(!cloudRefs.keySet().contains(i)){
+                            if (!cloudRefs.keySet().contains(i)) {
                                 db.collection("itemlabelcrossref_table").add(ref);
-                            }else{
+                            } else {
                                 db.collection("itemlabelcrossref_table").document(cloudRefs.get(i)).set(ref);
                             }
                         }
@@ -275,17 +278,17 @@ public class SyncFragment extends Fragment {
                 });
     }
 
-    public void updateTime(Map<String, String> uploadTime){
+    public void updateTime(Map<String, String> uploadTime) {
         db.collection("update_time")
                 .whereEqualTo("user", user.getUid())
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        if (task.getResult().size() > 0){
+                        if (task.getResult().size() > 0) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 db.collection("update_time").document(document.getId()).set(uploadTime);
                             }
-                        }else{
+                        } else {
                             db.collection("update_time").add(uploadTime);
                         }
                     } else {
@@ -294,21 +297,21 @@ public class SyncFragment extends Fragment {
                 });
     }
 
-    public void getTime(String timeName){
+    public void getTime(String timeName) {
 
         db.collection("update_time")
                 .whereEqualTo("user", user.getUid())
                 .get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful()) {
-                        if (task.getResult().size() > 0){
+                        if (task.getResult().size() > 0) {
                             for (QueryDocumentSnapshot document : task.getResult()) {
                                 db.collection("update_time").document(document.getId()).addSnapshotListener(getActivity(), new EventListener<DocumentSnapshot>() {
                                     @Override
                                     public void onEvent(@Nullable DocumentSnapshot value, @Nullable FirebaseFirestoreException error) {
-                                        if (timeName=="uploadTime"){
+                                        if (timeName == "uploadTime") {
                                             binding.uploadRecord.setText(value.getString(timeName));
-                                        }else if (timeName=="fetchTime"){
+                                        } else if (timeName == "fetchTime") {
                                             binding.fetchRecord.setText(value.getString(timeName));
                                         }
 
@@ -322,7 +325,7 @@ public class SyncFragment extends Fragment {
                 });
     }
 
-    public void fetchItemTable(){
+    public void fetchItemTable() {
 
         // get user's local items
         // check which of them are not in the cloud database, and remove in local
@@ -363,7 +366,7 @@ public class SyncFragment extends Fragment {
                 });
     }
 
-    public void fetchLabelTable(){
+    public void fetchLabelTable() {
 
         List<Label> localLabels = getLocalLabel();
         db.collection("label_table")
@@ -396,7 +399,7 @@ public class SyncFragment extends Fragment {
                 });
     }
 
-    public void fetchCrossRefTable(){
+    public void fetchCrossRefTable() {
 
         // get user's local items
         // check which of them are not in the cloud database, and remove in local
@@ -434,19 +437,19 @@ public class SyncFragment extends Fragment {
                 });
     }
 
-   public List<Item> getLocalItem() {
+    public List<Item> getLocalItem() {
 
-       List<Item> localItems = new ArrayList<>();
-       syncViewModel.getAllItems().observe(getViewLifecycleOwner(), new Observer<List<Item>>() {
-           @Override
-           public void onChanged(List<Item> items) {
-               localItems.addAll(items);
-           }
-       });
-       return localItems;
-   }
+        List<Item> localItems = new ArrayList<>();
+        syncViewModel.getAllItems().observe(getViewLifecycleOwner(), new Observer<List<Item>>() {
+            @Override
+            public void onChanged(List<Item> items) {
+                localItems.addAll(items);
+            }
+        });
+        return localItems;
+    }
 
-    public List<Label> getLocalLabel(){
+    public List<Label> getLocalLabel() {
         List<Label> localLabels = new ArrayList<>();
         syncViewModel.getAllLables().observe(getViewLifecycleOwner(), new Observer<List<Label>>() {
             @Override
@@ -454,10 +457,10 @@ public class SyncFragment extends Fragment {
                 localLabels.addAll(labels);
             }
         });
-       return localLabels;
-   }
+        return localLabels;
+    }
 
-    public List<ItemLabelCrossRef> getLocalCrossRef(){
+    public List<ItemLabelCrossRef> getLocalCrossRef() {
         List<ItemLabelCrossRef> localRefs = new ArrayList<>();
         syncViewModel.getAllCrossRefs().observe(getViewLifecycleOwner(), new Observer<List<ItemLabelCrossRef>>() {
             @Override
@@ -497,7 +500,7 @@ public class SyncFragment extends Fragment {
 
     }
 
-    public String getFileExtension (Uri uri){
+    public String getFileExtension(Uri uri) {
         ContentResolver cR = getContext().getContentResolver();
         MimeTypeMap mime = MimeTypeMap.getSingleton();
         return mime.getExtensionFromMimeType(cR.getType(uri));
@@ -505,35 +508,48 @@ public class SyncFragment extends Fragment {
 
     public void convertImageUrlToLocal(String url, Item item) {
 
-        if (ContextCompat.checkSelfPermission(getActivity(),
-                Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            saveImage(url, item);
-        }else{
-            askPermission();
-        }
+//        if (ContextCompat.checkSelfPermission(getActivity(),
+//                Manifest.permission.ACCESS_FINE_LOCATION)
+//                != PackageManager.PERMISSION_GRANTED) {
+//            saveImage(url, item);
+//        }else{
+//            askPermission();
+//        }
+        saveImage(url, item);
     }
 
-    private void askPermission(){
+    private void askPermission() {
         ActivityCompat.requestPermissions(getActivity(),
                 new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
                 1);
 
     }
 
-    private void saveImage(String url, Item item){
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+        // Save a file: path for use with ACTION_VIEW intents
+        return image;
+    }
+
+    private void saveImage(String url, Item item) {
         StorageReference itemRef = firebaseStorage.getReferenceFromUrl(url);
         try {
 
-            File localFile = File.createTempFile("images", ".jpg");
+            File localFile = createImageFile();
             Context context_1 = getContext();
             itemRef.getFile(localFile).addOnSuccessListener(taskSnapshot -> {
-                // Local temp file has been created
-                Bitmap photo = BitmapFactory.decodeFile(localFile.getAbsolutePath());
-                String contentUrl = MediaStore.Images.Media.insertImage(context_1.getContentResolver(), photo, "item" + item.getItemId() +".jpg", "item" + item.getItemId() + "image");
                 Toast.makeText(context_1, "Image successfully saved", Toast.LENGTH_SHORT).show();
-                syncViewModel.updateItemImageURL(item, contentUrl);
-
+                Uri photoUri = FileProvider.getUriForFile(getContext(), "com.example.digitalrefrige.fileprovider", localFile);
+                String imageUri = photoUri.toString();
+                syncViewModel.updateItemImageURL(item, imageUri);
             }).addOnFailureListener(new OnFailureListener() {
                 @Override
                 public void onFailure(@NonNull Exception exception) {
@@ -546,7 +562,6 @@ public class SyncFragment extends Fragment {
         }
 
 
-
     }
 
-    }
+}
